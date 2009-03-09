6714d4935d01233877320999be7180de3b488678
Stephen Hemminger <stephen.hemminger@vyatta.com>
2009-03-09 11:32:29 -0700

    Debian dfsg patch

--- include/ntp_md5.h
+++ include/ntp_md5.h
@@ -2,7 +2,12 @@
  * ntp_md5.h: deal with md5.h headers
  */
 
-#ifdef HAVE_MD5_H
+#ifdef OPENSSL
+# include <openssl/md5.h>
+#define MD5Init MD5_Init
+#define MD5Update MD5_Update
+#define MD5Final MD5_Final
+#elif defined(HAVE_MD5_H)
 # include <md5.h>
 #else
 # include "rsa_md5.h"
--- libntp/a_md5encrypt.c
+++ libntp/a_md5encrypt.c
@@ -16,13 +16,7 @@
 #include "ntp_string.h"
 #include "ntp_stdlib.h"
 
-/* Disable the openssl md5 includes, because they'd clash with ours. */
-/* #define NO_MD5 */
-/* #define OPENSSL_NO_MD5 */
-#undef OPENSSL
-
 #include "ntp.h"
-#include "global.h"
 #include "ntp_md5.h"
 
 /*
